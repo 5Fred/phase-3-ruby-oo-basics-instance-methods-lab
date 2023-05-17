4572291094722471c5c48575604b6c93814769e8
Fredrick <mwangikingz94@gmail.com>
2023-05-17 11:57:00 +0300

    done assignment

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+#1.dog.rb
+
+class Dog
+    def bark
+      puts "Woof!"
+    end
+  
+    def sit
+      puts "The Dog is sitting"
+    end
+  end
+  
+
+  
---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+#2.person.rb
+
+class Person
+    def talk
+      puts "Hello World!"
+    end
+  
+    def walk
+      puts "The Person is walking"
+    end
+  end
+  
